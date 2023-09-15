ed8659e403b17e049c700a8e39c6fa40df7f4eb7
Paul HoangLong <paulhoanglong@gmail.com>
2023-09-15 09:42:50 -0500

    Header and Footer setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,17 +23,16 @@ function App() {
         setGalleryList(response.data);
       })
       .catch((error) => {
-        console.log("error in GET App",error);
+        console.log("error in GET App", error);
       });
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Gallery of My Life</h1>
-      </header>
+      <Header />
       <p>Gallery goes here</p>
       <img src="images/goat_small.jpg" />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+      <footer className="App-footer">
+      </footer>
+    );
+  }
+  
+  export default Footer;
